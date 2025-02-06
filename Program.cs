b74a0b3ecd8a4332c8cacc49880c149edbf1a035
SERGIO HENRIQUE DA SILVA <sergiosilva01505@gmail.com>
2025-02-06 11:13:49 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadoraSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            int primeiroNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite outro número: ");
+            int segundoNum = Convert.ToInt32(Console.ReadLine());
+
+            int soma = primeiroNum + segundoNum;
+            int sub = primeiroNum - segundoNum;
+            int multi = primeiroNum * segundoNum;
+            int divisao = primeiroNum / segundoNum;
+
+            Console.WriteLine($"A soma entre {primeiroNum} + {segundoNum} = {soma}");
+            Console.WriteLine($"A subtração entre {primeiroNum} - {segundoNum} = {sub}");
+            Console.WriteLine($"A multiplicação entre {primeiroNum} X {segundoNum} = {multi}");
+            Console.WriteLine($"A divisão entre {primeiroNum} / {segundoNum} = {divisao}");
+        }
+    }
+}
